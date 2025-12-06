a7e4f82a6eb4347a163550f2bb770edd3dc56235
Chisadin <tranthanhnam.ccc@gmail.com>
2025-12-06 11:33:42 +0700

    support Discord RPC

--- main.ts
+++ main.ts
@@ -1,8 +1,73 @@
 import { app, BrowserWindow, nativeImage, shell } from "electron"
-import updater from "electron-updater"
+import updater from "electron-updater" 
 const autoUpdater = updater.autoUpdater
 
+import { Client } from "discord-rpc"
+
+const clientID = "1446675403581292706"
+
 const appicon = nativeImage.createFromPath("./assets/build/icon.png")
+
+const uptime = new Date()
+
+const rpcClient = new Client({transport: "ipc"})
+type RouteKey =
+  "/" | "/home" | "/login" | "/schedule" | "/timetable" | "/weather" |
+  "/diemdanh" | "/mark" | "/qrscan" | "/parking" | "/settings" | "*"
+
+const routeRPCMap: Record<RouteKey, { details: string; state: string }> = {
+  "/": { details: "Trang chính", state: "Dashboard" },
+  "/home": { details: "Trang chính", state: "Dashboard" },
+  "/login": { details: "Đăng nhập", state: "Xin quyền truy cập" },
+  "/schedule": { details: "Thời khóa biểu", state: "📅" },
+  "/timetable": { details: "Thời khóa biểu chi tiết", state: "📘" },
+  "/weather": { details: "Thời tiết", state: "🌤" },
+  "/diemdanh": { details: "Điểm danh", state: "🟢" },
+  "/mark": { details: "Xem điểm", state: "📊" },
+  "/qrscan": { details: "Quét QR", state: "📷" },
+  "/parking": { details: "Gửi xe", state: "🅿" },
+  "/settings": { details: "Cài đặt", state: "⚙" },
+  "*": { details: "Không xác định", state: "Lang thang 💀" },
+}
+
+
+rpcClient.on("ready", () => {
+    console.log(`Client ${clientID} ready`);
+
+})
+
+rpcClient.on("disconnected", () => {
+    console.log(`Client ${clientID} disconnected`);
+    
+})
+
+const setActivity = (path: string) => {
+    if (!rpcClient || !clientID) return
+
+    const key = (routeRPCMap[path as RouteKey] ? path : "*") as RouteKey
+    const data = routeRPCMap[key]
+
+    try {
+
+        rpcClient.setActivity({
+            details: data.details || "Đang xem lịch học",
+            state: data.state || "Đang xem lịch học",
+            startTimestamp: uptime,
+            largeImageKey: "appicon",
+            instance: true,
+            buttons: [
+                {label: "Truy cập LHU Dashboard", "url": "https://lhu-dashboard.chisadin.site"}
+            ]
+        }).then(() => {
+            console.log(`Updated RPC: ${path}`)
+        }).catch((error) => {
+            console.error('Error setting activity:', error);
+        })
+
+    } catch (error) {
+        console.error('Error setting activity:', error);
+    }
+}
 const createWindow = () => {
 
     const win = new BrowserWindow({
@@ -19,6 +84,17 @@ const createWindow = () => {
         return {action: "deny"}
     })
 
+    rpcClient.on("ready", () => {
+        console.log(`RPC connected: ${rpcClient.user?.username}`)
+        setActivity("/") 
+    })
+
+    win.webContents.on("did-navigate-in-page", (e, url) => {
+        
+        const path = new URL(url).pathname
+        setActivity(path)
+    })
+
     autoUpdater.checkForUpdatesAndNotify()
     
     win.setMenu(null)
@@ -28,6 +104,7 @@ const createWindow = () => {
 // Mấy cái dưới này để quản lý vòng đời của app, docs của electron bảo v 🐧🐧
 
 app.whenReady().then(() => {
+    rpcClient.login({ clientId: clientID }).catch(console.error)
     createWindow()
 })
 
